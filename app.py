b473ee2edd4cd3d27473f56bc4ffe6b8907363c3
PrajwalRReddy6 <prajwalrr99@gmail.com>
2024-05-25 07:15:37 +0000

    test changes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-  return "HellO World"
-
+  return render_template('home.html')
+  
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+  
 if __name__ == '__main__' :
   app.run(host='0.0.0.0',debug=True)
   
